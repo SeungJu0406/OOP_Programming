b8972418d23a2177e5fe0bc0429f94838d7f2d66
SeungJu0406 <nss8164@gmail.com>
2024-07-22 17:58:05 +0900

    생성자
    트레이너는 최대 6마리의 몬스터를 가질 수 있다.
    트레이너가 몬스터 생성 시 몬스터의 체력을 지정 값으로 초기화 해야 한다.
    Main 함수에서 트레이너 인스턴스 생성 시, 트레이너의 이름을 초기화 해야 한다.

---
+++ Constructor/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        class Trainer
+        {
+            public string name;
+            public Monster[] monsters;
+            public Trainer(string name)
+            {
+                this.name = name;
+                this.monsters = new Monster[6];
+            }
+        }
+        class Monster
+        {
+            public string name;
+            public int hp;
+            public Monster(string name, int hp) 
+            { 
+                this.name = name;
+                this.hp = hp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Trainer hanJiU = new Trainer("한지우");
+            hanJiU.monsters[0] = new Monster("피카츄", 35);
+            hanJiU.monsters[1] = new Monster("망나뇽", 91);
+            hanJiU.monsters[2] = new Monster("루카리오", 70);
+            hanJiU.monsters[3] = new Monster("창파나이트", 62);
+            hanJiU.monsters[4] = new Monster("팬텀", 60);
+            hanJiU.monsters[5] = new Monster("어래곤", 90);
+
+            Console.WriteLine($"트레이너: {hanJiU.name}");
+            for (int i = 0; i < hanJiU.monsters.Length; i++)
+            {
+                Console.WriteLine($"{i}. {hanJiU.monsters[i].name,-5}   {hanJiU.monsters[i].hp}");
+            }
+        }
+    }
+}
