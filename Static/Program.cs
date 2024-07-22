e33e93a1832319788578330728c4de868d939874
SeungJu0406 <nss8164@gmail.com>
2024-07-22 16:02:48 +0900

    Static: 계산기 클래스 생성
    덧셈, 뺄셈, 곱셈, 나눗셈, 제곱 기능
    나눗셈은 0을 나누었을떄 에러발생 메세지

---
+++ Static/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Static
+{
+    internal class Program
+    {
+        class MyCalculator
+        {
+            static public double Add(double a, double b)
+            {
+                return a + b; 
+            }
+            static public double Subtract(double a, double b)
+            {
+                return a - b;
+            }
+            static public double Multiply(double a, double b)
+            {
+                return a * b;
+            }
+            static public double Divide(double a, double b)
+            {
+                if (b == 0)
+                {
+                    Console.Write("에러 발생: ");
+                    return a/b;
+                }
+                return a / b;
+            }
+            static public double Squared(double a, double b) 
+            {
+                return Math.Pow(a,b);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyCalculator.Add(1.1, 3.3));
+            Console.WriteLine(MyCalculator.Subtract(5,1));
+            Console.WriteLine(MyCalculator.Multiply(3,4));
+            Console.WriteLine(MyCalculator.Divide(3,0));
+            Console.WriteLine(MyCalculator.Squared(2,5));
+        }
+    }
+}
