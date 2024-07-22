5fd7f5a2f350dae07c921cf4621b83564cecea26
SeungJu0406 <nss8164@gmail.com>
2024-07-22 16:16:09 +0900

    [refactor] MyCalculator.Divide 변경
    if 안의 retrun 삭제

--- Static/Program.cs
+++ Static/Program.cs
@@ -21,7 +21,6 @@
                 if (b == 0)
                 {
                     Console.Write("에러 발생: ");
-                    return a/b;
                 }
                 return a / b;
             }
@@ -35,7 +34,7 @@
             Console.WriteLine(MyCalculator.Add(1.1, 3.3));
             Console.WriteLine(MyCalculator.Subtract(5,1));
             Console.WriteLine(MyCalculator.Multiply(3,4));
-            Console.WriteLine(MyCalculator.Divide(3,0));
+            Console.WriteLine(MyCalculator.Divide(3,1));
             Console.WriteLine(MyCalculator.Squared(2,5));
         }
     }
