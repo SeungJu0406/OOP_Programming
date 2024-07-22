b48be894753a76100dde58003c893b56738ff1fe
SeungJu0406 <nss8164@gmail.com>
2024-07-22 15:45:47 +0900

    Chracter클래스 생성
    필드: 레벨, 체력, 이동속도, 공격력
    메서드: 전진, 후진, 좌회전, 우회전, 공격, 피격

---
+++ 240722/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _240722
+{
+    internal class Program
+    {
+        class Chracter
+        {
+            int level;
+            int hp;
+            int speed;
+            int power;
+            void MoveForward()
+            {
+                Console.WriteLine("전진하였습니다");
+            }
+            void MoveBackUp()
+            {
+                Console.WriteLine("후진하였습니다");
+            }
+            void MoveLeft()
+            {
+                Console.WriteLine("왼쪽으로 꺾었습니다");
+            }
+            void MoveRight()
+            {
+                Console.WriteLine("오른쪽으로 꺾었습니다");
+            }
+            void Attack()
+            {
+                Console.WriteLine("공격 하였습니다");
+            }
+            void Gethit()
+            {
+                Console.WriteLine("피격 당했습니다");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
