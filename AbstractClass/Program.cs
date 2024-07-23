4824897ef4e06697ff24ab17e1921da9779833d4
SeungJu0406 <nss8164@gmail.com>
2024-07-23 21:24:57 +0900

    [feat] 트레이너, 포켓몬 구현
    트레이너 포켓몬 6마리까지, 첫번째는 피카츄 초기값
    포켓몬 클래스로 피카츄, 이상해씨, 꼬부기, 파이리 구현

--- AbstractClass/Program.cs
+++ AbstractClass/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace AbstractClass
+﻿using TrainerSystem;
+
+namespace AbstractClass
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Trainer HanJiU = new Trainer();
+            Console.WriteLine($"{HanJiU.monsters[0].name} {HanJiU.monsters[0].level}");
+            HanJiU.monsters[0].BaseAttack();
         }
     }
 }
---
+++ AbstractClass/Trainer.cs
@@ -0,0 +1,72 @@
+﻿namespace TrainerSystem
+{
+    public enum MobType { Electric, Fire, Water, Grass }
+    public class Trainer
+    {
+        public Monster[] monsters;
+        public Trainer()
+        {
+            monsters = new Monster[6];
+            monsters[0] = new Pikachu(100, "삐까츄");
+        }
+    }
+    public abstract class Monster
+    {
+        public int level;
+        public MobType type;
+        public string name;
+        public abstract void BaseAttack();
+    }
+    public class Pikachu : Monster
+    {
+        public Pikachu(int level, string name)
+        {
+            this.level = level;
+            this.type = MobType.Electric;
+            this.name = name;
+        }
+        public override void BaseAttack()
+        {
+            Console.WriteLine("전광석화");
+        }
+    }
+    public class Squirtle : Monster
+    {
+        public Squirtle(int level, string name)
+        {
+            this.level = level;
+            this.type = MobType.Water;
+            this.name = name;
+        }
+        public override void BaseAttack()
+        {
+            Console.WriteLine("물대포");
+        }
+    }
+    public class Bulbasaur : Monster
+    {
+        public Bulbasaur(int level, string name)
+        {
+            this.level = level;
+            this.type = MobType.Grass;
+            this.name = name;
+        }
+        public override void BaseAttack()
+        {
+            Console.WriteLine("덩쿨채찍");
+        }
+    }
+    public class Charmander : Monster
+    {
+        public Charmander(int level, string name)
+        {
+            this.level = level;
+            this.type = MobType.Fire;
+            this.name = name;
+        }
+        public override void BaseAttack()
+        {
+            Console.WriteLine("화염방사");
+        }
+    }
+}
