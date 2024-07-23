3ad62e600b36ea480bcfadec466e93a5b7f57436
SeungJu0406 <nss8164@gmail.com>
2024-07-23 18:09:49 +0900

    메인함수 하스스톤 영웅 부분 주석처리, 4대특징 txt파일 추가

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -7,17 +7,17 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Hero[] heroes =
-            {
-                new Jaina(12),
-                new Garroshi(15),
-                new Anduin(99)
-            };
-            foreach (Hero hero in heroes) 
-            {
-                hero.HeroInfo();
-                hero.SpeakScript();
-            }
+            //Hero[] heroes =
+            //{
+            //    new Jaina(12),
+            //    new Garroshi(15),
+            //    new Anduin(99)
+            //};
+            //foreach (Hero hero in heroes) 
+            //{
+            //    hero.HeroInfo();
+            //    hero.SpeakScript();
+            //}
 
             RaidBoss[] bosses =
             {
