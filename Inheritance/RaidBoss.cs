e65aa15c0668eeda07a1da69d2dfdc1cc1f71c51
SeungJu0406 <nss8164@gmail.com>
2024-07-23 17:42:15 +0900

    신드라고사, 리치왕 클래스 생성
    생성자 및 드랍템 생성

--- Inheritance/RaidBoss.cs
+++ Inheritance/RaidBoss.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Inheritance
+﻿namespace RaidBoss
 {
-    internal class RaidBoss
+    public abstract class RaidBoss
     {
+        protected int hp;
+        protected int order;
+        protected Name name;
+        protected enum Name { Marrowgar , Sindragosa , RichKing}
+        public abstract void GetReward();
+    }
+    public class Sindragosa : RaidBoss 
+    {
+        public Sindragosa()
+        {
+            hp = 90000;
+            order = 11;
+            name = Name.Sindragosa;
+        }
+        public override void GetReward()
+        {
+            Console.WriteLine("매로우가르의 그러모은 목걸이");
+        }
+    }
+    public class RichKing : RaidBoss
+    {
+        public RichKing()
+        {
+            hp = 100000;
+            order = 12;
+            name = Name.RichKing;
+        }
+        public override void GetReward()
+        {
+            Console.WriteLine("푸른용군단의 잃어버린 파비스");
+        }
+    }
+    public class Marrowgar : RaidBoss
+    {
+        public Marrowgar()
+        {
+            hp = 10000;
+            order = 1;
+            name = Name.Marrowgar;
+        }
+        public override void GetReward()
+        {
+            Console.WriteLine("테레나스 2세의 왕실 홀");
+        }
     }
 }
