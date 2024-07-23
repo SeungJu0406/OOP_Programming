8c251f3de3ec91c0d1b8163f273599608533c02b
SeungJu0406 <nss8164@gmail.com>
2024-07-23 17:13:16 +0900

    하스스톤 영웅 자식클래스 생성
    제이나 가로쉬 안두인 생성
    영웅의 정보를 출력하는 함수 작성

--- Inheritance/HearthstoneHero.cs
+++ Inheritance/HearthstoneHero.cs
@@ -6,18 +6,66 @@ using System.Threading.Tasks;
 
 namespace HearthstoneHero
 {
-    public class Hero
+    public abstract class Hero
     {
-        Name name;
-        int hp;
-        Job job;
-        public enum Name { 제이나, 가로쉬, 안두인, empty}
-        public enum Job{마법사, 전사, 사제, empty }
-        public Hero()
+         protected int level;
+         protected Name name;
+         protected int hp;
+         protected Job job;
+        public enum Name { Jaina, Garroshi, Anduin, empty}
+        public enum Job { Mage, Warrior, Priest, empty }
+        public Hero(int level)
         {
+            this.level = level;
             this.name=Name.empty;
             this.hp = 30;
+            this.job= Job.empty;
+        }
+        public abstract void SpeakScript();
+        public void HeroInfo()
+        {
+            Console.WriteLine($"레벨: {level}, 이름: {name}, 직업: {job}, 체력: {hp}");
+        }
+    }
+    public class Jaina : Hero
+    {
+        public Jaina(int level):base(level) 
+        {
+            this.level =level;
+            this.name = Name.Jaina;
+            this.job = Job.Mage;
+        }
+        public override void SpeakScript() 
+        {
+            Console.WriteLine("후회하게 해 드리죠.");
+        }
+        
+    }
+    public class Garroshi : Hero
+    {
+        public Garroshi(int level) : base(level)
+        {
+            this.level = level;
+            this.name = Name.Garroshi;
+            this.job = Job.Warrior;
+        }
+        public override void SpeakScript()
+        {
+            Console.WriteLine("승리가 아니면 죽음을!");
+        }
 
+    }
+    public class Anduin : Hero
+    {
+        public Anduin(int level) : base(level)
+        {
+            this.level = level;
+            this.name = Name.Anduin;
+            this.job = Job.Priest;
+        }
+        public override void SpeakScript()
+        {
+            Console.WriteLine("빛이 당신을 태울 것 입니다!");
         }
 
     }
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Inheritance
+﻿using HearthstoneHero;
+
+namespace Inheritance
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hero[] heroes =
+            {
+                new Jaina(12),
+                new Garroshi(15),
+                new Anduin(99)
+            };
+            foreach (Hero hero in heroes) 
+            {
+                hero.HeroInfo();
+            }
         }
     }
 }
