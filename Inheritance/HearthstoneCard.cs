e03081d9c6a71f6d401a04c634b672d5bf73722e
SeungJu0406 <nss8164@gmail.com>
2024-07-23 20:23:14 +0900

    [feat] 하스스톤 카드의 자식클래스 생성
    하늘빛비룡 용암거인 리로이젠킨스 생성

--- Inheritance/HearthstoneCard.cs
+++ Inheritance/HearthstoneCard.cs
@@ -4,9 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inheritance
+namespace HearthstoneCard
 {
-    internal class HearthstoneCard
+    public abstract class Card
     {
+        protected enum Name { Shieldbearer, AzureDrake, MoltenGiant, LeeroyJenkins }
+        protected enum Rarity {Common,Rare, Epic, Legendary}
+        protected Name name;
+        protected int power;
+        protected int hp;
+        protected int cost;
+        protected Rarity rarity;
+        protected bool isGoldenCard;
+        public Card()
+        {
+            bool isGoldenCard = false;
+        }
+        public abstract void PrintEffect();
+        public virtual void PrintGoldenEffect()
+        {
+            Console.WriteLine("일반카드입니다. 빛나지 않습니다");
+        }
+
     }
+    public class Shieldbearer : Card
+    {
+        public Shieldbearer()
+        {
+            name = Name.Shieldbearer;
+            power = 0;
+            hp = 4;
+            cost = 1;
+            rarity = Rarity.Common;
+        }
+        public override void PrintEffect()
+        {
+            Console.WriteLine("카드가 빛나지 않습니다");
+        }
+    }
+    public class AzureDrake : Card
+    {
+        public AzureDrake()
+        {
+            name = Name.AzureDrake;
+            power = 4;
+            hp = 4;
+            cost = 5;
+            rarity = Rarity.Rare;
+        }
+        public override void PrintEffect()
+        {
+            Console.WriteLine("카드가 파란색으로 빛납니다");
+        }
+    }
+    public class MoltenGiant : Card
+    {
+        public MoltenGiant()
+        {
+            name = Name.MoltenGiant;
+            power = 8;
+            hp = 8;
+            cost = 20;
+            rarity = Rarity.Epic;
+        }
+        public override void PrintEffect()
+        {
+            Console.WriteLine("카드가 보라색으로 빛납니다...!!!");
+        }
+    }
+    public class LeeroyJenkins : Card
+    {
+        public LeeroyJenkins()
+        {
+            name = Name.LeeroyJenkins;
+            power = 6;
+            hp = 2;
+            cost = 5;
+            rarity = Rarity.Legendary;
+            isGoldenCard = true;
+        }
+        public override void PrintEffect()
+        {
+            Console.WriteLine("카드가!!!!!!! 황금색으로!!!!!! 빛납니다!!!!!!");
+        }
+        public override void PrintGoldenEffect()
+        {
+            Console.WriteLine("황금카드입니다. 매우매우 반짝반짝 빛나며, 일러스트가 움직입니다.");
+        }
+    }
+
 }
