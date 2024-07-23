33bb78736c719f8ffe73dc66e6b9321a7fca7c36
SeungJu0406 <nss8164@gmail.com>
2024-07-23 20:36:54 +0900

    테스트 줄 나눔 표시 추가

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -4,35 +4,37 @@ using HearthstoneCard;
 
 namespace Inheritance
 {
-    //
+    // 
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Hero[] heroes =
-            //{
-            //    new Jaina(12),
-            //    new Garroshi(15),
-            //    new Anduin(99)
-            //};
-            //foreach (Hero hero in heroes) 
-            //{
-            //    hero.HeroInfo();
-            //    hero.SpeakScript();
-            //}
-
-            //RaidBoss[] bosses =
-            //{
-            //    new Marrowgar(),
-            //    new Sindragosa(),
-            //    new RichKing()
-            //};
-            //foreach(RaidBoss boss in bosses)
-            //{
-            //    boss.BossInfo();
-            //    boss.GetReward();
-            //}
-
+            Hero[] heroes =
+            {
+                new Jaina(12),
+                new Garroshi(15),
+                new Anduin(99)
+            };
+            foreach (Hero hero in heroes)
+            {
+                hero.HeroInfo();
+                hero.SpeakScript();
+                Console.WriteLine();
+            }
+            Console.WriteLine("=========================================");
+            RaidBoss[] bosses =
+            {
+                new Marrowgar(),
+                new Sindragosa(),
+                new RichKing()
+            };
+            foreach (RaidBoss boss in bosses)
+            {
+                boss.BossInfo();
+                boss.GetReward();
+                Console.WriteLine();
+            }
+            Console.WriteLine("=========================================");
             Card[] cards =
             {
                 new Shieldbearer(),
