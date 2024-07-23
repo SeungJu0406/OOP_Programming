e9527dfb31178bc698acdacc6b6acc4813f39cd4
SeungJu0406 <nss8164@gmail.com>
2024-07-23 16:37:33 +0900

    [feat] 하스스톤 영웅 클래스 생성
    필드: 이름 체력 직업 작성

---
+++ Inheritance/HearthstoneHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthstoneHero
+{
+    public class Hero
+    {
+        Name name;
+        int hp;
+        Job job;
+        public enum Name { 제이나, 가로쉬, 안두인, empty}
+        public enum Job{마법사, 전사, 사제, empty }
+        public Hero()
+        {
+            this.name=Name.empty;
+            this.hp = 30;
+
+        }
+
+    }
+}
