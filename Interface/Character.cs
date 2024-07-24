959699460d1e422adac6398729a4d41b598d1d35
SeungJu0406 <nss8164@gmail.com>
2024-07-24 22:40:35 +0900

    [feat] 전투의 함성 하수인 작성

--- Interface/Character.cs
+++ Interface/Character.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interface
+﻿namespace Interface
 {
-    public class Character : ITakeDamage, IAttack, ICounter ,IDie
+    public class Character : ITakeDamage, IAttack, IDie
     {
-        public string name; 
+        public string name;
         public int power;
         public int hp;
         public Character(string name, int power)
@@ -24,9 +18,9 @@ namespace Interface
                 Console.WriteLine($"{name}은/는 {target.name}에게 공격할 수 없습니다");
                 return;
             }
-            Console.WriteLine($"{name}이/가 {target.name}에게 {power}만큼의 피해를 입혔습니다");
+            Console.WriteLine($"{name}이/가 {target.name}에게 {power}만큼의 피해를 입혔습니다.");
             target.TakeDamege(power);
-            target.Counter(this);
+            TakeDamege(target.power);
         }
 
         public void TakeDamege(int power)
@@ -39,18 +33,9 @@ namespace Interface
                 return;
             }
         }
-        public void Counter(Character attacker)
-        {
-            if (hp > 0)
-            {
-                Console.WriteLine($"{name}이/가 {attacker.name}에게 반격합니다");
-                Attack(attacker);
-            }
-        }
-        
         public virtual void Die()
         {
-            Console.WriteLine($"{name}이 죽었습니다");            
+            Console.WriteLine($"{name}이 죽었습니다");
         }
     }
 
@@ -72,8 +57,21 @@ namespace Interface
         public override void Die()
         {
             base.Die();
-            team.RemoveTeam(this);          
+            team.RemoveTeam(this);
         }
         private Team<Character> team; // Team<T>와 연동하기 위한 변수
+    }
+    public class BattleCryMinion : Minion // 전투의 함성 하수인
+    {
+        public BattleCryMinion(string name, int power, int hp, string description, Team<Character> team) : base(name, power, hp, team)
+        {
+            this.battleCryDescription = description;
+            Battlecry();
+        }
+        public void Battlecry()
+        {
+            Console.WriteLine($"{name}의 {battleCryDescription} 전투의 함성 효과!");
+        } 
+        protected string battleCryDescription;
     }
 }
--- Interface/Program.cs
+++ Interface/Program.cs
@@ -6,13 +6,13 @@
         {
             Team<Character> members = new(8);
             members.AddTeam(new Hero("가로쉬", 4)); // 0
-            members.AddTeam(new Minion("하늘빛 비룡", 4, 4,members)); // 1
+            members.AddTeam(new Minion("꿀주먹 오우거", 6, 7,members)); // 1
             members.AddTeam(new Minion("단검 곡예사", 3, 2, members)); // 2
-            members.AddTeam(new Minion("험상궃은 손님", 3, 3, members)); // 3
+            members.AddTeam(new Minion("볼바르 폴드라곤", 1, 7, members)); // 3
             members.AddTeam(new Minion("방패병", 0, 4, members)); // 4
             members.AddTeam(new Minion("박사 붐", 7, 7, members)); // 5
-            members.AddTeam(new Minion("리로이 젠킨스", 6, 2, members)); // 6
-            members.AddTeam(new Minion("위습", 1, 1, members)); // 7
+            members.AddTeam(new BattleCryMinion("리로이 젠킨스", 6, 2,"적에게 새끼용 두마리를 소환합니다",members)); // 6
+            members.AddTeam(new BattleCryMinion("바위언덕 수호병", 1, 5,"도발 하수인을 발견합니다" ,members)); // 7
             members.AddTeam(new Minion("용암 거인", 8, 8, members)); // 8
             Console.WriteLine();
             members.members[4].Attack(members.members[1]);
@@ -22,6 +22,8 @@
             members.members[0].Attack(members.members[4]);
             Console.WriteLine();
             members.members[5].Attack(members.members[1]);
+            Console.WriteLine();
+            members.members[7].Attack(members.members[3]);
             Console.WriteLine();
             members.PrintCharacter();
         }
