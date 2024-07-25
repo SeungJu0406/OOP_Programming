6d256c284ab8faf4553364143e5f41ebef0f16f1
SeungJu0406 <nss8164@gmail.com>
2024-07-25 17:57:45 +0900

    확장메소드 과제 작성

---
+++ Additision/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Additision
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("아이디를 입력하세요 : ");
+            string id = Console.ReadLine();
+
+            if (id.IsAllowedID())
+            {
+                Console.WriteLine("ID가 유효합니다.");
+            }
+            else
+            {
+                Console.WriteLine("ID에 허용되지 않는 특수문자가 있습니다.");
+            }
+        }
+        public static bool IsAllowedID(this string id) 
+        {
+            foreach(char c in id)
+            {
+                // 아스키코드로 변환해서 숫자랑 알파벳에 해당되지 않으면 반환값 false
+                if (!((48 <= (int)c && (int)c <= 57) || (65 <= (int)c && (int)c <= 90) || (97 <= (int)c && (int)c <= 122))) 
+                    return false;
+            }
+            return true;
+        }
+    }
+}
