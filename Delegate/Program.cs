574899b48b36422ae4e8c99701934e9e1b4bd320
SeungJu0406 <nss8164@gmail.com>
2024-07-25 14:30:17 +0900

    [docs] 추가 설명 주석 추가

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -10,21 +10,21 @@
             public void Equip(Armor armor)
             {
                 Console.WriteLine($"플레이어가 {armor.name} 을/를 착용합니다.");
-                curArmor = armor;
+                curArmor = armor; // 현재 인스턴스 curArmor필드에 armor라는 Armor 인스턴스 객체 착용 : 캐릭터가 장비를 착용
             }
 
             public void UnEquip()
             {
                 Console.WriteLine($"플레이어가 {curArmor.name} 을/를 해제합니다.");
-                curArmor = null;
+                curArmor = null; // 현재 인스턴스 curArmor필드 비우기 : 캐릭터 장비 벗기
             }
 
             public void Hit()
             {
                 Console.WriteLine("플레이어가 데미지를 받았습니다");
-                if(OnHit != null)
+                if(OnHit != null) 
                 {
-                    OnHit();
+                    OnHit(); // 나 맞아따!!!!!!!!!!! 
                 }
             }
         }
@@ -44,10 +44,10 @@
 
             public void DecreaseDurability()
             {
-                durability--;               
-                if (durability <= 0)
+                durability--; // 내구도 1 감소
+                if (durability <= 0) // 내구도 0되면
                 {
-                    Break();
+                    Break(); // 장비 파괴
                     return;
                 }
                 Console.WriteLine($"장비의 내구도가 닳습니다. 현재 내구도: {durability}");
@@ -58,7 +58,7 @@
                 if(OnBreaked != null)
                 {
                     Console.WriteLine("장비가 파괴되었습니다.");
-                    OnBreaked();
+                    OnBreaked(); // 나 부숴져따!!!!!!!!!!
                 }
             }
         }
@@ -67,13 +67,13 @@
         {
             Player player = new Player();
             Armor ammor = new Armor("갑옷", 3);
+            player.OnHit += ammor.DecreaseDurability; // 맞으면 내구도 깎일게
+            ammor.OnBreaked += player.UnEquip; // 부숴지면 벗을게
 
             player.Equip(ammor);
-            player.OnHit += ammor.DecreaseDurability;
-            ammor.OnBreaked += player.UnEquip;
-            player.Hit();
-            player.Hit();
-            player.Hit();
+            player.Hit(); // 1대
+            player.Hit(); // 2대
+            player.Hit(); // 3대
         }
     }
 }
