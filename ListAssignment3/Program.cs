37dcac0f59bdb06e7ae2fab4f44b6134ea3fdf50
SeungJu0406 <nss8164@gmail.com>
2024-07-28 23:01:54 +0900

    [feat] List<T> 배열 추가, 특정 요소 삭제
    배열 크기 초과시 새로운 배열에 넣는것과 몇번 인덱스 삭제, 배열 비우기 넣어야됨

--- ListAssignment3/Program.cs
+++ ListAssignment3/Program.cs
@@ -4,34 +4,57 @@
     {
         static void Main(string[] args)
         {
-                      
+            MyList<int> myList = new MyList<int>();
+            for (int i = 0; i < 8; i++)
+            {
+                myList.Add(i);
+            }
+            myList.Remove(4);
+            myList.Remove(5);
+            myList.PrintList();
         }
     }
     public class MyList<T>
     {
         T[] list;
         int index;
-        public MyList() 
+        public MyList()
         {
             index = 0;
-            list = new T[10];           
+            list = new T[10];
+        }
+        private MyList(MyList<T> list)
+        {
+            this.index = list.index;
+            this.list = new T[list.list.Length * 2];
         }
         public void Add(T element)
         {
-            if (index > list.Length) 
+            if (index > list.Length)
             {
-                list = AddList();
+                this.list = new MyList<T>(this).list;
             }
             list[index++] = element;
-            
         }
-        public void AddList()
+        public void Remove(T element)
         {
-            
-        }
-        public void Remove()
-        {
-
+            T temp;
+            bool removeElement = false;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].Equals(element))
+                {
+                    removeElement = true;
+                }
+                if (removeElement)
+                {
+                    if (i+1>=list.Length)
+                        break;
+                    list[i] = list[i + 1];
+                }
+               
+            }
+            index--;
         }
         public void RemoveAt()
         {
@@ -41,5 +64,13 @@
         {
 
         }
+        public void PrintList()
+        {
+            foreach (T element in list)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine($"배열 총량: {list.Length}");
+        }
     }
-}
+}    
