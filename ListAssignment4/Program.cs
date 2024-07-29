14236009484fee5adca67d8adce2159793c2cf8b
SeungJu0406 <nss8164@gmail.com>
2024-07-29 15:31:59 +0900

    [feat] 연결리스트 단방향 노드 연결 구현
    링크드리스트 클래스에서 Next만 구현됬음

---
+++ ListAssignment4/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ListAssignment4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> myList = new();
+            LinkedListNode<int> node1 = new LinkedListNode<int>(1);
+            LinkedListNode<int> node2 = node1.AddNext(2);
+            LinkedListNode<int> node3 = node2.AddNext(4);
+            Console.WriteLine(node2.next.element);
+
+        }
+    }
+    public class LinkedList<T>
+    {
+        LinkedList<T> list;
+        LinkedListNode<T> first;
+        LinkedListNode<T> last;
+        public LinkedList()
+        {
+            first = null;
+            list = this;
+            last = null;
+        }
+        public void AddFisrt()
+        {
+
+        }
+        public void AddLast(LinkedListNode<T> node,T element)
+        {
+           
+        }
+
+    }
+    public class LinkedListNode<T>
+    {
+        public T element { get; set; }
+        public LinkedListNode<T> next { get; set; }
+        LinkedListNode<T> previois;
+        public LinkedListNode(T node)
+        {
+            element = node;
+            next = null;
+            previois = null;
+        }
+        public LinkedListNode(T node, LinkedListNode<T> prev)
+        {
+            element = node;
+            next = null;
+            previois = prev;
+        }
+        public LinkedListNode<T> AddNext(T value)
+        {
+            next = new LinkedListNode<T>(value, this);
+            return next;
+        }
+    } 
+}
