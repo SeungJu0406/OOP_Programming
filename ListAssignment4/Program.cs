fd7f345eef93a062d1a21abca9fd56a0cc98b02a
SeungJu0406 <nss8164@gmail.com>
2024-07-29 16:08:41 +0900

    [feat] 연결리스트 AddFirst,AddLast,리스트 전체 출력 기능 구현

--- ListAssignment4/Program.cs
+++ ListAssignment4/Program.cs
@@ -1,59 +1,103 @@
-﻿namespace ListAssignment4
+﻿using System.Xml.Linq;
+
+namespace ListAssignment4
 {
     public class Program
     {
         static void Main(string[] args)
         {
             LinkedList<int> myList = new();
-            LinkedListNode<int> node1 = new LinkedListNode<int>(1);
-            LinkedListNode<int> node2 = node1.AddNext(2);
-            LinkedListNode<int> node3 = node2.AddNext(4);
-            Console.WriteLine(node2.next.element);
-
+            myList.AddLast(1);
+            myList.AddLast(2);
+            myList.AddLast(3);
+            myList.AddFirst(0);
+            myList.AddFirst(6);
+            myList.AddLast(7);
+            myList.PrintList();
         }
     }
     public class LinkedList<T>
     {
-        LinkedList<T> list;
-        LinkedListNode<T> first;
-        LinkedListNode<T> last;
+        public LinkedList<T> list { get; set; }
+        public LinkedListNode<T> first { get; set; }
+        public LinkedListNode<T> last { get; set; }
+        public LinkedListNode<T> node { get; set; }
         public LinkedList()
         {
-            first = null;
             list = this;
+            first = null;
             last = null;
+            node = null;
         }
-        public void AddFisrt()
+        public void AddFirst(T value)
         {
-
+            if (node == null)
+            {
+                node = new LinkedListNode<T>(value);
+                last = node;
+                first = last;
+            }
+            node = first.AddPrev(value);
+            node.next = first; 
+            first = first.previous;
         }
-        public void AddLast(LinkedListNode<T> node,T element)
+        public void AddLast(T value)
         {
-           
+            if(node == null)
+            {
+                node = new LinkedListNode<T>(value);
+                last = node;
+                first = last;
+                return;
+            }
+            node = last.AddNext(value);
+            node.previous= last;
+            last = last.next;
         }
-
+        public void PrintList()
+        {
+            FuncPrintList(first);
+        }
+        public LinkedListNode<T> FuncPrintList(LinkedListNode<T> node)
+        {
+            if (node == null)
+                return null;
+            Console.WriteLine($"{node.element}");
+            return FuncPrintList(node.next);
+        }
     }
     public class LinkedListNode<T>
     {
         public T element { get; set; }
         public LinkedListNode<T> next { get; set; }
-        LinkedListNode<T> previois;
+        public LinkedListNode<T> previous { get; set; }
         public LinkedListNode(T node)
         {
             element = node;
             next = null;
-            previois = null;
+            previous = null;
         }
         public LinkedListNode(T node, LinkedListNode<T> prev)
         {
             element = node;
             next = null;
-            previois = prev;
+            previous = prev;
+        }
+        public LinkedListNode(T node, LinkedListNode<T> next, int dummy)
+        {
+            element = node;
+            next = next;
+            previous = null;
         }
         public LinkedListNode<T> AddNext(T value)
         {
             next = new LinkedListNode<T>(value, this);
             return next;
+        }
+        public LinkedListNode<T> AddPrev(T value)
+        {
+            previous = new LinkedListNode<T>(value, this, 1);           
+            return previous;
         }
     } 
 }
