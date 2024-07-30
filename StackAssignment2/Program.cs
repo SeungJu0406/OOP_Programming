3aed30699cb3be7c3c6978509286acee56542409
SeungJu0406 <nss8164@gmail.com>
2024-07-30 15:40:13 +0900

    [feat] Stack 클래스 구현

--- StackAssignment2/Program.cs
+++ StackAssignment2/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-
+            Stack<int> stack = new();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+            stack.Push(5);
+            //stack.PrintStack();
         }
     }
     public class Stack<T>
@@ -15,12 +24,12 @@
         public Stack()
         {
             index = 0;
-            stack = new T[10];           
+            stack = new T[10];
         }
         public Stack(int pastIndex, Stack<T> pastStack)
         {
             index = pastIndex;
-            stack = new T[pastStack.stack.Length*2];
+            stack = new T[pastStack.stack.Length * 2];
         }
         public void Push(T value)
         {
@@ -28,21 +37,33 @@
             {
                 temp = stack;
                 stack = new Stack<T>(index, this).stack;
-                for(int i = 0; i < temp.Length; i++)
+                for (int i = 0; i < temp.Length; i++)
                 {
                     stack[i] = stack[i];
                 }
                 Push(value);
                 return;
             }
-            stack[index++]=value;
+            stack[index++] = value;
         }
         public void Pop()
         {
-
+            stack[index--] = default(T);
         }
-
-            
+        public T Peek()
+        {
+            return stack[index-1];
+        }
+        public void Clear()
+        {
+            Array.Clear(stack, 0, stack.Length);
+        }
+        public void PrintStack()
+        {
+            foreach (T t in stack)
+            {
+                Console.WriteLine(t);
+            }
+        }
     }
-
 }
