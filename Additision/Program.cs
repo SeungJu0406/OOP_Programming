e0ab26c3b8bac7ac57c50898eb347aca8c707dee
SeungJu0406 <nss8164@gmail.com>
2024-07-30 16:36:06 +0900

    [refactor] 아스키코드 문자, 숫자 논리식 변환
    아스키코드 직접 변환에서, IsLetterOrDigit로 바꿨음

--- Additision/Program.cs
+++ Additision/Program.cs
@@ -23,7 +23,8 @@ namespace Additision
             foreach(char c in id)
             {
                 // 아스키코드로 변환해서 숫자랑 알파벳에 해당되지 않으면 반환값 false
-                if (!((48 <= (int)c && (int)c <= 57) || (65 <= (int)c && (int)c <= 90) || (97 <= (int)c && (int)c <= 122))) 
+                //if (!((48 <= (int)c && (int)c <= 57) || (65 <= (int)c && (int)c <= 90) || (97 <= (int)c && (int)c <= 122))) 
+                if(!(char.IsLetterOrDigit(c))) // char.IsLetterOrDigit(c): c안의 값이 문자값 혹은 숫자값이면 true
                     return false;
             }
             return true;
