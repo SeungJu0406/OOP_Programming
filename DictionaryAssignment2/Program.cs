2e2e80d44440f1c3d661b9366aa980bd64d78da1
SeungJu0406 <nss8164@gmail.com>
2024-08-02 00:09:09 +0900

    [feat] 딕셔너리 Add 구현중

--- DictionaryAssignment2/Program.cs
+++ DictionaryAssignment2/Program.cs
@@ -4,20 +4,43 @@
     {
         static void Main(string[] args)
         {
-            char test = '할';
-            int test1 = test;
-            Console.WriteLine(test1);
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("테스트", 123);
         }
     }
-    public class Dictionary<TKey, TValue> 
+    public class Dictionary<TKey, TValue>
     {
-        TKey[] key;
-        TValue[] value;
-        public Dictionary() 
+        TKey?[] key;
+        TValue?[] value;
+        int index;
+
+        public Dictionary()
         {
-            key = new TKey[193];
-            value = new TValue[193];
+            key = new TKey?[193];
+            value = new TValue?[193];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            index=FuncHash(key);
+            this.key[index] = key;
+            this.value[index] = value;
         }
 
-    }    
+
+        private int FuncHash(dynamic key)
+        {
+            if (key is int)
+                return key % this.key.Length;
+            else if (key is float)
+                return (int)(key % this.key.Length);
+            else if (key is double)
+                return (int)(key % this.key.Length);
+            else if (key is char)
+                return (int)key % this.key.Length;
+            else if (key is string)
+                return (int)key[0] % this.key.Length;
+            else
+                return key % this.key.Length;
+        }
+    }
 }
