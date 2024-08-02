b90ac0dc6e01c43e7e9b1bfc8602697bff388249
SeungJu0406 <nss8164@gmail.com>
2024-08-02 10:47:27 +0900

    [feat] 레벨테스트 1번 문제 구현
    Swap 함수 구현, int, long, float, double, chat, string에 적용 가능

---
+++ LevelTest/LevelTest1/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace LevelTest1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int iLeft = 10;
+            int iRight = 20;
+            Util.Swap(ref iLeft, ref iRight);
+            Console.WriteLine("int 자료형을 사용한 Swap 함수");
+            Console.WriteLine($"{iLeft}  {iRight}");
+
+            Console.WriteLine();
+
+            double dLeft = 3.5;
+            double dRight = 8.8;
+            Util.Swap(ref dLeft, ref dRight);
+            Console.WriteLine("double 자료형을 사용한 Swap 함수");
+            Console.WriteLine($"{dLeft}  {dRight}");
+
+            Console.WriteLine();
+
+            string sLeft = "Left";
+            string sRight = "Right";
+            Util.Swap(ref sLeft, ref sRight);
+            Console.WriteLine("string 자료형을 사용한 Swap 함수");
+            Console.WriteLine($"{sLeft}  {sRight}");
+        }
+    }
+    public static class Util
+    {
+        public static void Swap(ref int left, ref int right)
+        {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+        public static void Swap(ref long left, ref long right)
+        {
+            long temp = left;
+            left = right;
+            right = temp;
+        }
+        public static void Swap(ref float left, ref float right)
+        {
+            float temp = left;
+            left = right;
+            right = temp;
+        }
+        public static void Swap(ref double left, ref double right)
+        {
+            double temp = left;
+            left = right;
+            right = temp;
+        }
+        public static void Swap(ref char left, ref char right)
+        {
+            char temp = left;
+            left = right;
+            right = temp;
+        }
+        public static void Swap(ref string left, ref string right)
+        {
+            string temp = left;
+            left = right;
+            right = temp;
+        }
+    }
+
+}
