b1c74155cbcadc9540c03b3974792e1fbe35b7da
SeungJu0406 <nss8164@gmail.com>
2024-08-02 22:35:37 +0900

    [feat] 딕셔너리 재해싱 구현 실패
    힌트 받아요

--- DictionaryAssignment2/Program.cs
+++ DictionaryAssignment2/Program.cs
@@ -1,31 +1,43 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace DictionaryAssignment2
+﻿namespace DictionaryAssignment2
 {
     public class Program
     {
+        // 재해싱 도대체 어떻게하는거죠
+        // 배열을 늘리고 재해싱해서 값을 다시 넣은뒤 새로운 배열에서 key값을 해싱하면 해당 인덱스가 다르게 나옵니다. 도대체 이유를 모르겠습니다
+        // 힌트,,,,,!!!
         static void Main(string[] args)
         {
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
             dictionary.Add("테스트", 123);
             dictionary.Add("테이프", 456);
             Console.WriteLine(dictionary.Contains("테스트"));
-            dictionary.Remove("테스트");
-            dictionary.Remove("테이프");
             dictionary.Add("테란", 784);
-            Console.WriteLine(dictionary.Contains("테란"));
+            dictionary.Add("일번", 4561);
+            dictionary.Add("이번", 1235);
+            dictionary.Add("삼번", 1271617);
+            Console.WriteLine(dictionary.Contains("테란")); 
+            Console.WriteLine(dictionary.Find("테란"));
+            Console.WriteLine(dictionary.Find("삼번"));
+            dictionary.Remove("삼번");
+            Console.WriteLine(dictionary.Contains("삼번"));
         }
     }
     public class Dictionary<TKey, TValue>
     {
         Key<TKey, TValue>[] keyArr;
+        Key<TKey, TValue>[] temp;
         int index;
+        int count;
         public Dictionary()
         {
             keyArr = new Key<TKey, TValue>[193];
         }
+        public Dictionary(int size)
+        {
+            keyArr = new Key<TKey, TValue>[size * 4];
+        }
         public void Add(TKey key, TValue value)
-        { 
+        {
             index = FuncHash(key);
             while (keyArr[index] != null)
             {
@@ -34,6 +46,7 @@ namespace DictionaryAssignment2
                     index = 0;
             }
             keyArr[index] = new Key<TKey, TValue>(key, value);
+            count++;
         }
 
         public void Remove(TKey key)
@@ -50,16 +63,35 @@ namespace DictionaryAssignment2
                 }
                 if (keyArr[index].key.Equals(key))
                     break;
-
                 index++;
                 if (keyArr[index] == null)
                     return;
             }
             keyArr[index].key = default(TKey);
-            keyArr[index].value = default(TValue);
+            keyArr[index].value = default(TValue); 
+            count--;
         }
 
         public bool Contains(TKey key)
+        {
+            index = FuncHash(key);
+            while (keyArr[index] != null)
+            {
+                if (index >= keyArr.Length)
+                    index = 0;
+                if (keyArr[index].key == null)
+                {
+                    index++;
+                    continue;
+                }
+                if (keyArr[index].key.Equals(key))
+                    return true;
+                index++;
+            }
+            return false;
+        }
+
+        public TValue Find(TKey key)
         {
             index = FuncHash(key);
             while (keyArr[index] != null)
@@ -76,29 +108,18 @@ namespace DictionaryAssignment2
 
                 index++;
                 if (keyArr[index] == null)
-                    return false;
+                    return default(TValue);
             }
-            return true;
+            return keyArr[index].value;
         }
 
-        private int FuncHash(dynamic key)
+        private int FuncHash(TKey key)
         {
-            if (key is int)
-                return key % this.keyArr.Length;
-            else if (key is float)
-                return (int)(key % this.keyArr.Length);
-            else if (key is double)
-                return (int)(key % this.keyArr.Length);
-            else if (key is char)
-                return (int)key % this.keyArr.Length;
-            else if (key is string)
-                return (int)key[0] % this.keyArr.Length;
-            else
-                return (int)key % this.keyArr.Length;
+            int hash = key.GetHashCode();
+            hash = Math.Abs(hash);
+            return hash%keyArr.Length;
         }
     }
-
-
     public class Key<Tkey, TValue>
     {
         public Tkey key;
