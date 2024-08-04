e4ff3d29bbda0ade4101544a78365e229309b936
SeungJu0406 <nss8164@gmail.com>
2024-08-04 20:53:35 +0900

    [refactor] 딕셔너리 static 으로 변환

--- Dictionary/DictionaryAssignment1/Program.cs
+++ Dictionary/DictionaryAssignment1/Program.cs
@@ -4,45 +4,38 @@
     {
         static void Main(string[] args)
         {
-            MonsterData monsterData = new MonsterData();
-            Monster mantis = new Monster(monsterData, "사마귀");
-            Console.WriteLine($"{mantis.name}: {mantis.hp}");
-
+            MonsterData.SaveData();
+            Monster mentis = new Monster("사마귀");
+            Console.WriteLine($"{mentis.name}, {mentis.hp}");
         }
     }
-    public class MonsterData
+    public static class MonsterData
     {
-        public Dictionary<string, MonsterData> monsterData;
-        public string name {  get; set; }
-        public int hp { get; set; }
-        
-        public MonsterData()
+        public static Dictionary<string, Monster> monsterData;        
+        public static void SaveData() 
         {
-            monsterData = new Dictionary<string, MonsterData>();
-            SaveData();
-        }
-        private MonsterData(string name, int hp) 
-        {
-            this.name = name;
-            this.hp = hp;
-        }
-        private void SaveData() 
-        {
-            monsterData.Add("달팽이", new MonsterData("달팽이",5));
-            monsterData.Add("사마귀", new MonsterData("사마귀", 10));
-            monsterData.Add("말벌", new MonsterData("말벌", 8));
-            monsterData.Add("사슴벌레", new MonsterData("사슴벌레", 30));
-            monsterData.Add("장수풍뎅이", new MonsterData("장수풍뎅이", 35));
+            monsterData = new Dictionary<string, Monster>();
+            monsterData.Add("달팽이", new Monster("달팽이", 5));
+            monsterData.Add("사마귀", new Monster("사마귀", 10));
+            monsterData.Add("말벌", new Monster("말벌", 8));
+            monsterData.Add("사슴벌레", new Monster("사슴벌레", 30));
+            monsterData.Add("장수풍뎅이", new Monster("장수풍뎅이", 35));
         }
     }
     public class Monster 
     {
         public string name;
-        public int hp; 
-        public Monster(MonsterData data, string key)
+        public int hp;
+        public Monster(string name)
         {
-            this.name = data.monsterData[key].name;
-            this.hp = data.monsterData[key].hp;
+            Monster data = MonsterData.monsterData[name];
+            this.name = data.name;
+            this.hp = data.hp;
+        }
+        public Monster(string name , int hp)
+        {
+            this.name = name;
+            this.hp = hp;
         }
     }
 }
